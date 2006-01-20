f526e46fa2e8f19be9bcc5914e05323dbf741d4e
Wayne Meisner <wayne.meisner@opengear.com>
2006-01-20 01:18:56 +0000

    More L&F manipulations

--- src/sdtconnector/Main.java
+++ src/sdtconnector/Main.java
@@ -8,10 +8,11 @@
  */
 
 package sdtconnector;
+import javax.swing.JFrame;
 import javax.swing.UIManager;
 import com.jgoodies.looks.Options;
 import com.jgoodies.looks.LookUtils;
-        
+
 
 /**
  *
@@ -29,11 +30,27 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         try {
-            String lafName;
-            lafName = UIManager.getSystemLookAndFeelClassName();
-            //lafName = Options.getSystemLookAndFeelClassName();
+            String lafName = System.getProperty("swing.defaultlaf");
+            System.out.println("Default laf = " + lafName);
+            if (lafName != null) {
+                // Just use whatever the user wanted
+            
+            } else if (LookUtils.IS_OS_WINDOWS_XP || LookUtils.IS_OS_MAC
+                    || LookUtils.IS_JAVA_6_OR_LATER) {
+                lafName = UIManager.getSystemLookAndFeelClassName();
+            } else if (LookUtils.IS_OS_WINDOWS) {
+                lafName = Options.PLASTICXP_NAME;
+                lafName = Options.getSystemLookAndFeelClassName();
+            } else {
+                // Use The Looks L&F on pre-1.6 java on linux, since
+                // the pre 1.6 GTK L&F did not work that well
+                lafName = "com.birosoft.liquid.LiquidLookAndFeel";
+                lafName = Options.PLASTICXP_NAME;
+            }
+            System.out.println("Using " + lafName + " look & feel");
             UIManager.setLookAndFeel(lafName);
         } catch (Exception e) {}
+        
         new MainWindow().setVisible(true);
     }
     
