ca16895a213787e9a3af86ed6550d3b0e4b77553
Wayne Meisner <wayne.meisner@opengear.com>
2006-03-09 12:24:59 +0000

    Add loading/saving of lists

--- SDTConnector/src/sdtconnector/Settings.java
+++ SDTConnector/src/sdtconnector/Settings.java
@@ -5,6 +5,12 @@
 
 package sdtconnector;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
@@ -20,6 +26,33 @@ public class Settings {
     }
     public static void setProperty(String id, String value) {
         settings.put(id, value);
+    }
+    public static Collection getPropertyList(String id) {
+        Preferences list = settings.node(id);
+        try {
+            List<String> ret = new LinkedList<String>();
+            for (String elemID : list.keys()) {
+                String val = list.get(elemID, "");
+                ret.add(val);
+            }
+            return ret;
+        } catch (BackingStoreException ex) {
+            return Collections.emptyList();
+        }
+    }
+    public static void setPropertyList(String id, Collection l) {
+        int count = 0;
+        Preferences list = settings.node(id);
+        try {
+            list.clear();
+            for (Iterator i = l.iterator(); i.hasNext(); ++count) {
+                Object o = i.next();
+                list.put(String.valueOf(count), o.toString());
+            }
+            list.flush();
+        } catch (BackingStoreException ex) {
+            ex.printStackTrace();
+        }        
     }
     public static String[] keys() {
         try {
