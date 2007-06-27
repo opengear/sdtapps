849ed83eeae4d50e00d3549f1f42169e5669c9ff
Robert Waldie <robert.waldie@opengear.com>
2007-06-27 06:24:02 +0000

    Bumped version to 1.4.1.

--- SDTConnector/src/sdtconnector/SDTConnector.java
+++ SDTConnector/src/sdtconnector/SDTConnector.java
@@ -11,7 +11,7 @@ package sdtconnector;
  *
  */
 public class SDTConnector {
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
     /** Creates a new instance of SDTConnector */
     private SDTConnector() {
     }
