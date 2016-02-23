06ca5de074e1032c490ad304d6d18661dd7d4da3
David Leonard <david.leonard@opengear.com>
2016-02-23 23:27:41 +1000

    Version 1.7.5

--- SDTConnector/src/sdtconnector/SDTConnector.java
+++ SDTConnector/src/sdtconnector/SDTConnector.java
@@ -15,7 +15,7 @@ public class SDTConnector {
     // Version format is major.minor.patch - bump patch each release,
     // bump minor for updated default preferences, bump major for changes
     // that break backwards compatibility
-    public static final String VERSION = "1.7.4";
+    public static final String VERSION = "1.7.5";
 
     public static final boolean DEBUG = false;
     
